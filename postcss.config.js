d4aeda8290ab836d882a2d3c12f93496b5163bc6
Alexandre <amoutonbrady@gmail.com>
2020-01-26 02:48:27 +0100

    :sparkles: Adding parcel 2

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
+// only needed if you want to purge
+const purgecss = require('@fullhuman/postcss-purgecss')({
+	content: ['./src/**/*.js', './src/**/*.html'],
+	defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
 module.exports = {
-	plugins: [require('tailwindcss')],
+	plugins: [
+		require('tailwindcss'),
+
+		// only needed if you want to purge
+		...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+	],
 };
